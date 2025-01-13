570f6079a38e3b25bc3cdff14bf27db47ecd3722
abdellah711 <alaouiabdellah711@gmail.com>
2025-01-13 18:29:32 +0100

    setup dependencies

---
+++ db/columns.ts
@@ -0,0 +1,7 @@
+// reusable db columns
+import { timestamp } from "drizzle-orm/pg-core";
+
+export const timestamps = {
+  updatedAt: timestamp("updated_at"),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+};
