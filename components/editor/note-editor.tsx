dcd58796dae8cb91ad5a2d2ccec32e80154c9371
abdellah711 <alaouiabdellah711@gmail.com>
2025-01-19 13:28:35 +0100

    feat: implement auto-saving functionality for note editor

--- components/editor/note-editor.tsx
+++ components/editor/note-editor.tsx
@@ -4,44 +4,101 @@ import { Plate } from "@udecode/plate/react";
 
 import { useCreateEditor } from "@/components/editor/use-create-editor";
 import { Editor, EditorContainer } from "@/components/plate-ui/editor";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import type { Note } from "@/db/schema/note";
+import { useMutation } from "@tanstack/react-query";
+import isEqual from "lodash-es/isEqual";
+import { updateNote } from "@/db/actions";
+import { formatRelative } from "date-fns";
+import { SidebarTrigger } from "../ui/sidebar";
 
 type Props = {
   note: Note;
 };
 
-export function NoteEditor({ note }: Props) {
-  const editor = useCreateEditor({ value: note.content as any });
+const SAVING_DELAY = 1000;
+
+export function NoteEditor({ note: initialNote }: Props) {
+  const editor = useCreateEditor({ value: initialNote.content as any });
   const editorRef = useRef<HTMLDivElement>(null);
+  const {
+    mutateAsync: saveNote,
+    variables: savedNote = initialNote,
+    isPending,
+    data = initialNote,
+  } = useMutation({ mutationFn: updateNote });
+  const [note, setNote] = useState(initialNote);
+
+  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNote({ ...note, title: e.target.value });
+  };
+
+  const isTheSameNote =
+    note.title === savedNote.title && isEqual(note.content, savedNote.content);
+
+  useEffect(() => {
+    if (isTheSameNote) {
+      return;
+    }
+    const timer = setTimeout(() => {
+      saveNote({
+        noteId: note.noteId,
+        title: note.title ?? "",
+        content: editor.children,
+      });
+    }, SAVING_DELAY);
+    return () => clearTimeout(timer);
+  }, [note]);
 
   return (
-    <div className="size-full px-12 pb-24 pt-4 text-base sm:px-[max(30px,calc(50%-350px))]">
-      <Plate editor={editor} onValueChange={({ value }) => console.log(value)}>
-        <input
-          placeholder="Untitled"
-          className="outline-none text-4xl font-[inherit] bg-transparent font-bold text-foreground-800 my-7 placeholder:font-normal"
-          onKeyDown={(event) => {
-            if (event.key === "Enter") {
-              event.preventDefault();
-              editorRef.current?.focus();
-            }
-          }}
-        />
-        <EditorContainer>
-          <Editor
-            variant="none"
-            placeholder="Type something..."
-            ref={editorRef}
-            className="text-foreground-700 pb-24"
-            onKeyDown={(e) => {
-              if (e.ctrlKey && e.key === "b") {
-                e.stopPropagation();
+    <>
+      <header className="flex pt-4 shrink-0 items-center gap-2">
+        <div className="flex items-center gap-2 px-4">
+          <SidebarTrigger className="-ml-1" />
+        </div>
+      </header>
+      <div className="size-full px-12 pb-24 pt-4 text-base sm:px-[max(30px,calc(50%-350px))]">
+        <p className="text-sm text-foreground-500 text-end mt-5 mb-2">
+          {isPending
+            ? "Saving..."
+            : isTheSameNote
+            ? `Saved at ${formatRelative(
+                data?.updatedAt ?? data.createdAt,
+                Date.now()
+              )}`
+            : "Unsaved changes"}
+        </p>
+        <Plate
+          editor={editor}
+          onValueChange={({ value }) => setNote({ ...note, content: value })}
+        >
+          <input
+            placeholder="Untitled"
+            className="outline-none text-4xl font-[inherit] bg-transparent font-bold text-foreground-800 mb-7 placeholder:font-normal"
+            value={note.title ?? ""}
+            onChange={handleTitleChange}
+            onKeyDown={(event) => {
+              if (event.key === "Enter") {
+                event.preventDefault();
+                editorRef.current?.focus();
               }
             }}
           />
-        </EditorContainer>
-      </Plate>
-    </div>
+          <EditorContainer>
+            <Editor
+              variant="none"
+              placeholder="Type something..."
+              ref={editorRef}
+              className="text-foreground-700 pb-24"
+              onKeyDown={(e) => {
+                if (e.ctrlKey && e.key === "b") {
+                  e.stopPropagation();
+                }
+              }}
+            />
+          </EditorContainer>
+        </Plate>
+      </div>
+    </>
   );
 }
--- db/actions.ts
+++ db/actions.ts
@@ -7,6 +7,7 @@ import { db } from ".";
 import { noteTable } from "./schema/note";
 import { and, eq } from "drizzle-orm";
 import { cache } from "react";
+import { z } from "zod";
 
 const getSession = cache(async () => {
   const session = await auth.api.getSession({
@@ -60,4 +61,33 @@ export const getNote = async (noteId: string) => {
   }
 
   return note[0];
+};
+
+const UpdateNoteSchema = z.object({
+  noteId: z.number().int(),
+  title: z.string(),
+  content: z.array(z.any()),
+});
+
+export type UpdateNote = z.infer<typeof UpdateNoteSchema>;
+
+export const updateNote = async (data: UpdateNote) => {
+  const note = UpdateNoteSchema.parse(data);
+  const session = await getSession();
+
+  return await db
+    .update(noteTable)
+    .set({
+      title: note.title,
+      content: note.content,
+      updatedAt: new Date(),
+    })
+    .where(
+      and(
+        eq(noteTable.noteId, note.noteId),
+        eq(noteTable.userId, session.user.id)
+      )
+    )
+    .returning()
+    .then((res) => res[0]);
 };
