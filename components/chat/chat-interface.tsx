094cdc9fd718bb2b74fdea00770b806964e57f41
abdellah711 <alaouiabdellah711@gmail.com>
2025-01-28 17:55:07 +0100

    feat: enhance chat interface with loading state

--- components/chat/chat-interface.tsx
+++ components/chat/chat-interface.tsx
@@ -1,6 +1,6 @@
 import { cn } from "@/lib/utils";
 import { Textarea, Button, Alert } from "@nextui-org/react";
-import { useChat } from "ai/react";
+import { Message, useChat } from "ai/react";
 import { SendIcon } from "lucide-react";
 import Markdown from "markdown-to-jsx";
 import { KeyboardEventHandler } from "react";
@@ -8,24 +8,39 @@ import { KeyboardEventHandler } from "react";
 type Props = {};
 
 export default function ChatInterface({}: Props) {
-  const { messages, input, handleInputChange, handleSubmit, error, reload } =
-    useChat({
-      initialMessages: [
-        {
-          id: "1",
-          content: "Hello! How can I help you?",
-          role: "assistant",
-        },
-      ],
-      maxSteps: 3,
-      onError: console.error,
-    });
+  const {
+    messages,
+    input,
+    handleInputChange,
+    handleSubmit,
+    error,
+    reload,
+    isLoading,
+    stop,
+  } = useChat({
+    initialMessages: [
+      {
+        id: "1",
+        content: "Hello! How can I help you?",
+        role: "assistant",
+      },
+    ],
+    maxSteps: 3,
+    onError: console.error,
+  });
 
   const handleKeyDown: KeyboardEventHandler = (e) => {
     if (e.key === "Enter" && !e.shiftKey) {
       handleSubmit(e);
     }
   };
+
+  const handleStop = (e: React.MouseEvent<HTMLButtonElement>) => {
+    if (!isLoading) return;
+    e.preventDefault();
+    stop();
+  };
+
   return (
     <>
       <div className="mb-3 px-5">
@@ -64,19 +79,7 @@ export default function ChatInterface({}: Props) {
           </Alert>
         )}
         {messages.toReversed().map((message) => (
-          <div
-            key={message.id}
-            className={cn(
-              "py-2 selection:bg-primary-300 prose prose-sm dark:prose-invert",
-              {
-                "bg-primary-100 self-end px-3 rounded-lg max-w-[80%]":
-                  message.role === "user",
-                "justify-start": message.role === "assistant",
-              }
-            )}
-          >
-            <Markdown>{message.content}</Markdown>
-          </div>
+          <MessageItem key={message.id} message={message} />
         ))}
       </div>
       <form className="flex gap-2 w-full mt-4" onSubmit={handleSubmit}>
@@ -90,10 +93,41 @@ export default function ChatInterface({}: Props) {
           maxRows={3}
           onKeyDown={handleKeyDown}
         />
-        <Button type="submit" isIconOnly color="primary">
-          <SendIcon size={20} />
+        <Button
+          type="submit"
+          isIconOnly
+          color="primary"
+          radius="full"
+          onClick={handleStop}
+        >
+          {isLoading ? (
+            <div className="size-3.5 bg-current rounded-[2px] animate-in" />
+          ) : (
+            <SendIcon size={20} />
+          )}
         </Button>
       </form>
     </>
+  );
+}
+
+type MessageProps = {
+  message: Message;
+};
+
+function MessageItem({ message }: MessageProps) {
+  return (
+    <div
+      className={cn(
+        "py-2 selection:bg-primary-300 prose prose-sm dark:prose-invert",
+        {
+          "bg-primary-100 self-end px-3 rounded-lg max-w-[80%]":
+            message.role === "user",
+          "justify-start": message.role === "assistant",
+        }
+      )}
+    >
+      <Markdown>{message.content}</Markdown>
+    </div>
   );
 }
